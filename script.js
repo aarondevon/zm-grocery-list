337fe88e2382d3681f8f8610eba1fccfa415d622
Aaron Sawyer <me@aarondevon.com>
2018-12-05 08:49:23 -0600

    add done class to li elements

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var li = document.querySelectorAll("li");
 
 function inputLength() {
 	return input.value.length;
@@ -23,6 +24,17 @@ function addListAfterKeypress(event) {
 	if (inputLength() > 0 && event.keyCode === 13) {
 		createListElement();
 	}
+}
+
+// loop through li to add done class
+for (let i = 0; i < li.length; i++) {
+	li[i].addEventListener('click', function() {
+		if (li[i].classList.contains("done")) {
+			li[i].classList.remove('done');
+		} else {
+			li[i].classList.add('done');
+		}
+	})
 }
 
 button.addEventListener("click", addListAfterClick);
